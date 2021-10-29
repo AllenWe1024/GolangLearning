5dfe4436793384568dbf6870dd016ea8db01d245
ZhuoranTian <849597043@qq.com>
2021-10-29 18:13:01 +0800

    错误查看

--- GolangPractice/test1.go
+++ GolangPractice/test1.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt" //注释样例
 
-func main() {
+func main(){
 	/*多行注释样例*/
 	fmt.Println("Hello, World!") //test1
+	//错误写法
+	// a :=1
+	// b :=2
+	// if(a && b){
+	// ...
+	// }
+	// fmt.Println(a|b);
+	// fmt.Println(a&b);
+
 }
